8a5ed9e3529d835a73115322569e39c145c0dfa3
DHRUVA GANESH HEGDE <dhruvaghegde@gmail.com>
2026-08-25 23:15:03 +0530

    Time: 0 ms (100%), Space: 13.5 MB (19.46%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -3,8 +3,8 @@ public:
     vector<int> shuffle(vector<int>& nums, int n) {
         vector<int>ans;
         for(int i=0;i<n;i++){
-            ans.push_back(nums[i]);//
-            ans.push_back(nums[i+n]);
+            ans.push_back(nums[i]);//takes the first element from the first half
+            ans.push_back(nums[i+n]);//takes the first element from the second half
         }
         return ans;
     }
